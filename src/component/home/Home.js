00f5af9f2fe8553bd47c5b5bbf4a083adf3ca38d
Divyank Mishra <divy58mishra@gmail.com>
2022-07-07 21:20:14 +0530

    useEffect Home page

--- src/component/home/Home.js
+++ src/component/home/Home.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CreatePost from "../post/CreatePost";
 import PostList from "../post/PostList";
 import "./Home.css";
 
 function Home(props) {
+  const [posts, setposts] = useState([]);
+  useEffect(() => {
+    setposts(props.home.posts);
+  }, [props.home.posts]);
+
   return (
     <section className="grid-container-home">
       <div className="home-col home-col-1">Column 1</div>
       <div className="home-col home-col-2">
         <CreatePost />
-        <PostList posts={props.home.posts} />
+        <PostList posts={posts} />
       </div>
       <div className="home-col home-col-3">Column 3</div>
     </section>
